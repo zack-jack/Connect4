326cd0b1e1fc7564b49f10653f08c46f344a9e03
zackjacksongit <z.jackson58@gmail.com>
2018-09-15 18:50:42 -0400

    Game class

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,19 @@
 class Game {
-  constructor() {}
+  constructor() {
+    this.board = new Board();
+    this.players = createPlayers();
+    this.ready = false;
+  }
+
+  /**
+   * Creates two player objects
+   * @return  {Array}    An array of two Player objects.
+   */
+  createPlayers() {
+    const players = [
+      new Player("Player 1", 1, "#e74c3c", true),
+      new Player("Player 2", 2, "#2980b9")
+    ];
+    return players;
+  }
 }
